c16b75ac3c81471517cd308eacc11f6627a06025
artem961 <artemkhrabrov03@gmail.com>
2024-12-14 22:05:50 +0300

    update

--- INFORMATICS/лр_5/task_3.py
+++ INFORMATICS/лр_5/task_3.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import pandas
 import seaborn
 
-df = pandas.read_csv("task_2.csv", delimiter=";")
+dataf = pandas.read_csv("task_2.csv", delimiter=";")
 
-df_melted = df.melt(
+dataf_melted = dataf.melt(
     id_vars="Date",
     value_vars=["Open", "Max", "Min", "Close"],
     var_name="Type",
@@ -13,17 +13,16 @@ df_melted = df.melt(
 
 plt.figure(figsize=(10, 6))
 seaborn.boxplot(
-    data=df_melted,
+    data=dataf_melted,
     x="Date",
     y="Value",
     hue="Type",
-    palette=["blue", "orange", "gray", "yellow"]
+    palette=["blue", "red", "green", "yellow"]
 )
 
 plt.title("Статистика")
 plt.ylabel("Значение")
 plt.xlabel("Дата")
-plt.legend(loc="upper right")
+plt.legend(loc="upper left")
 
 plt.savefig("task_3.png")
-plt.show()
