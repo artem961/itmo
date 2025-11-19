9d008ba5a8f030264dc30c57eccc93e1c71602bc
artem961 <artemkhrabrov03@gmail.com>
2025-11-19 16:23:00 +0300

    taglib try to use

--- WEB/lab3/taglib/src/main/java/taglib/components/accordion/AccordionSection.java
+++ WEB/lab3/taglib/src/main/java/taglib/components/accordion/AccordionSection.java
@@ -1,6 +1,7 @@
 package taglib.components.accordion;
 
 import jakarta.faces.component.FacesComponent;
+import jakarta.faces.component.UIComponent;
 import jakarta.faces.component.UIComponentBase;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.context.ResponseWriter;
@@ -9,18 +10,48 @@ import java.io.IOException;
 
 @FacesComponent("accordionSection")
 public class AccordionSection extends UIComponentBase {
+    private String title;
+
     @Override
     public String getFamily() {
         return "accordion";
     }
 
     @Override
-    public void encodeAll(FacesContext context) throws IOException {
+    public void encodeBegin(FacesContext context) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
-        writer.startElement("h", this);
+        initAttributes();
 
-        writer.writeText("text", null);
+        // Accordion header
+        writer.startElement("div", this);
+        writer.writeAttribute("class", "accordion-header", null);
 
-        writer.endElement("h");
+        writer.startElement("h3", this);
+        writer.writeText(title, null);
+        writer.endElement("h3");
+
+        writer.endElement("div");
+
+        // Accordion content
+        writer.startElement("div", this);
+        writer.writeAttribute("class", "accordion-content", null);
+    }
+
+    @Override
+    public void encodeChildren(FacesContext context) throws IOException {
+        // Рендерим дочерние компоненты (контент секции)
+        for (UIComponent child : getChildren()) {
+            child.encodeAll(context);
+        }
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        writer.endElement("div"); // закрываем accordion-content
+    }
+
+    private void initAttributes() {
+        this.title = (String) getAttributes().get("title");
     }
 }
